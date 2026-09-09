feae8cdea11fd288e331ccdd215fa1e2baa69d78
AdanIrfan <176397003+AdanIrfan@users.noreply.github.com>
2026-09-09 23:31:51 +0500

    Data layer of the todo list done

--- src/task.js
+++ src/task.js
@@ -1,39 +1,86 @@
-export function task(name, description, status, date, priority){
-    this.id = crypto.randomUUID();
-    this.name = name;
-    this.description = description;
-    this.status = status;
-    this.date = date;
-    this.priority = priority
-}
-
-export function project(name, tasks = []){
-    this.name = name;
-    this.tasks = tasks;
-}
-
-export const projectManager = (function(){
-    let projects = [];
-
-    addProject(type){
-        const newProject = project(type);
-        projects.push(newProject);
+    export function task(name, description, status, date, priority){
+        this.id = crypto.randomUUID();
+        this.name = name;
+        this.description = description;
+        this.status = status;
+        this.date = date;
+        this.priority = priority
     }
 
-    removeProject(type){
-        for(const item of projects){
-            if(item.name === type){
-                let index = projects.indexOf("item");
-                projects = projects.splice(index, 1);
+    export function project(name){
+        this.name = name;
+        this.tasks = [];
+
+        this.addTask = function(task){
+            this.tasks.push(task);
+        }
+
+        this.removeTask = function(task){
+            for(const item of this.tasks){
+                if(item.id === task.id ){
+                    const index = this.tasks.indexOf(item);
+                    this.tasks.splice(index, 1);
+                    return;
+                }
+            }
+        }
+
+        this.findTask = function(taskId){
+            for(const item of this.tasks){
+                if(item.id === taskId){
+                    return item;
+                }
             }
         }
     }
 
-    findProject(type){
-       for(const item of projects){
-            if(item.name === type){
-                return item;
+    export const projectManager = (function(){
+        let projects = [];
+        const defaultProject = project("default");
+        projects.push(defaultProject);
+
+        const getProjects = () => projects;
+
+        
+        function addProject(type){
+            const newProject = project(type);
+            projects.push(newProject);
+            return newProject;
+        }
+
+        function removeProject(type){
+            for(const project of projects){
+                if(project.name === type){
+                    let index = projects.indexOf(project);
+                    projects.splice(index, 1);
+                }
             }
-        } 
-    }
-})();
+        }
+
+        function findProject(type){
+        for(const project of projects){
+                if(project.name === type){
+                    return project;
+                }
+            } 
+        }
+        return {getProjects, addProject, removeProject, findProject};
+    })();
+
+
+    export function createTask(name, description, status, date, priority){
+        const activity = task(name, description, status, date, priority);
+        
+        const selectProject = prompt("which project do you want to go");
+        
+        let matchedProject = projectManager.findProject(selectProject);
+           if(matchedProject){
+                matchedProject.addTask(activity);
+           }
+           else{
+                console.log("project does not exist");
+
+                let newProject = projectManager.addProject(selectProject);
+                newProject.addTask(activity);
+           }
+}
